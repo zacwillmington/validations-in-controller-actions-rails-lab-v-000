69f061f7689527a4909e5b0a1c538b63a3d11299
Zac Willmington <zac.willmington@gmail.com>
2018-07-08 18:50:33 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
     validates :title, presence: true
     validates :category, acceptance: {  accept: "Fiction" || "Non-Fiction"}
+    validates :category, presence: true
     validates :content, length: { minimum: 100 }
 end
